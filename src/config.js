37f03554c0e28ca27bd14999bc3c1336ef4975f8
Chetan Kumar <Chetansain86@gmail.com>
2022-03-16 17:23:54 +0000

    feat: add config properties

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-  serverUrl: process.env.REACT_APP_SERVER_URL
+  serverUrl: process.env.REACT_APP_SERVER_URL,
+  guestEmail: process.env.REACT_APP_GUEST_EMAIL,
+  guestPassword: process.env.REACT_APP_GUEST_PASSWORD,
 }
